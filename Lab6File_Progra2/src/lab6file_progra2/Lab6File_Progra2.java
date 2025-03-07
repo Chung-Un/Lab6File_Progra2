3e4f256ecd64ca9f64c3a9a05604c08c78a6ed1b
Chung-Un <chungunyum23@gmail.com>
2025-03-07 13:26:07 -0600

    Inicio

---
+++ Lab6File_Progra2/src/lab6file_progra2/Lab6File_Progra2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab6file_progra2;
+
+/**
+ *
+ * @author chung
+ */
+public class Lab6File_Progra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
